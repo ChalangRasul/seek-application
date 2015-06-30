d59a9bb58ad3334946cb83cc6cef4ad41c791ffa
Brett Dulan <brett.dulan@outlook.com>
2015-06-29 20:45:24 -0400

    server errors

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var db = mongoose.connection;
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 7001;
 
 // set the view engine to ejs
 app.set('view engine', 'html');
