07dc5eb4ac76c2a1de6e8b5fdf2b443ce8a28e74
Brett Dulan <brett.dulan@outlook.com>
2015-07-14 23:09:35 -0400

    Fixed all changes

--- server.js
+++ server.js
@@ -1,12 +1,5 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname + '/public')).listen(1337);
-
 var express = require('express');
 var app = express();
-var mongoose = require('mongoose');
-
-var db = mongoose.connection;
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
@@ -20,10 +13,10 @@ app.use(express.static(__dirname + '/public'));
 
 // set the home page route
 app.get('/', function(req, res) {
-	
+
     res.render('index');
 });
 
 app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
+    console.log('Our app is running on PORT:' + port);
 });
